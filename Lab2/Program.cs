b7aa9a32d957e127370de22ee616871da82acd0b
Алексей Федоров <vedopov2014@gmail.com>
2024-09-16 13:20:42 +0300

    Lab2

---
+++ Lab2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод размеров матрицы
+        int n;
+        while (true)
+        {
+            Console.Write("Введите количество строк (n): ");
+            if (int.TryParse(Console.ReadLine(), out int result))
+            {
+                n = result;
+                break;
+            }
+            Console.WriteLine("Это не число. Повторите ввод.");
+        }
+        int m;
+        while (true)
+        {
+            Console.Write("Введите количество столбцов (m):");
+            if (int.TryParse(Console.ReadLine(), out int result))
+            {
+                m = result;
+                break;
+            }
+            Console.WriteLine("Это не число. Повторите ввод.");
+        }
+
+        // Инициализация матрицы
+        int[,] matrix = new int[n, m];
+
+        // Ввод элементов матрицы
+        Console.WriteLine("Введите элементы матрицы:");
+        int maxValue = int.MinValue;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                
+                while (true)
+                {
+                    Console.Write($"Элемент [{i},{j}]: ");
+                    if (int.TryParse(Console.ReadLine(),out int result))
+                    {
+                        matrix[i, j] = result;
+                        break;
+                    }
+                    Console.WriteLine("Это не число. Повторите ввод.");
+                }
+
+                // Определяем максимальное значение в процессе ввода
+                if (matrix[i, j] > maxValue)
+                    maxValue = matrix[i, j];
+            }
+        }
+
+        // Замена всех максимальных значений на нули
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (matrix[i, j] == maxValue)
+                    matrix[i, j] = 0;
+            }
+        }
+
+        // Вывод итоговой матрицы
+        Console.WriteLine("Матрица после замены максимальных значений на нули:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48a43fa1495ae6a4156e0a06df446eb776cc1783")]
-[assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
