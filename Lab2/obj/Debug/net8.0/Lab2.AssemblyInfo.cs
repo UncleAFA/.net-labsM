44a6d0b99f227cc0a7514bb23ebce8c04217ffc5
Алексей Федоров <vedopov2014@gmail.com>
2024-10-07 14:26:32 +0300

    lab6

--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3f3b1079ca21dd0f809567ce0c2c475592450e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6793f2b2da806402e53729e9c2c6bfdd30ad57ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab6/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EvenMatrixElements
+{
+    // Определение делегата, который будет использоваться для фильтрации
+    public delegate bool FilterDelegate(int number);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ввод размера матрицы от пользователя
+            Console.Write("Введите количество строк матрицы: ");
+            int rows = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите количество столбцов матрицы: ");
+            int columns = int.Parse(Console.ReadLine());
+
+            // Создание и заполнение матрицы случайными числами
+            int[,] matrix = GenerateRandomMatrix(rows, columns);
+            Console.WriteLine("Сгенерированная матрица:");
+            PrintMatrix(matrix);
+
+            // Получение четных элементов с помощью делегата
+            List<int> evenNumbers = FilterMatrix(matrix, IsEven);
+            Console.WriteLine("\nЧетные элементы матрицы:");
+            foreach (var number in evenNumbers)
+            {
+                Console.Write(number + " ");
+            }
+        }
+
+        // Метод для генерации матрицы случайных чисел
+        static int[,] GenerateRandomMatrix(int rows, int columns)
+        {
+            Random random = new Random();
+            int[,] matrix = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = random.Next(1, 100); // Генерация чисел от 1 до 99
+                }
+            }
+            return matrix;
+        }
+
+        // Метод для печати матрицы
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // Метод для фильтрации элементов матрицы с использованием делегата
+        static List<int> FilterMatrix(int[,] matrix, FilterDelegate filter)
+        {
+            List<int> filteredNumbers = new List<int>();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (filter(matrix[i, j]))
+                    {
+                        filteredNumbers.Add(matrix[i, j]);
+                    }
+                }
+            }
+            return filteredNumbers;
+        }
+
+        // Метод для проверки, является ли число четным
+        static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
